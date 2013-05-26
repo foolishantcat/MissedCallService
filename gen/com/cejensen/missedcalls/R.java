52626de392eea8e11e8815ab12b11cc82d501ae0
Claus Erik Jensen <claus.e.jensen@gmail.com>
2013-05-26 12:28:26 +0200

    Fixed lint warnings

--- gen/com/cejensen/missedcalls/R.java
+++ gen/com/cejensen/missedcalls/R.java
@@ -16,58 +16,48 @@ public final class R {
     }
     public static final class attr {
     }
-    public static final class dimen {
-        public static final int widget_margin=0x7f060000;
-    }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int about=0x7f0a0005;
-        public static final int button1=0x7f0a0003;
-        public static final int editLog=0x7f0a0002;
-        public static final int scrollView1=0x7f0a0001;
-        public static final int settings=0x7f0a0004;
-        public static final int startButton=0x7f0a0000;
+        public static final int about=0x7f090004;
+        public static final int editLog=0x7f090002;
+        public static final int scrollView1=0x7f090001;
+        public static final int settings=0x7f090003;
+        public static final int startButton=0x7f090000;
     }
     public static final class layout {
         public static final int activity_missed_call=0x7f030000;
-        public static final int appwidget_layout=0x7f030001;
     }
     public static final class menu {
-        public static final int activity_missed_call=0x7f090000;
+        public static final int activity_missed_call=0x7f080000;
     }
     public static final class string {
-        public static final int about=0x7f070002;
-        public static final int active=0x7f070009;
-        public static final int app_name=0x7f070000;
-        public static final int email_not_sent=0x7f070007;
-        public static final int email_sent=0x7f070008;
-        public static final int exit=0x7f07000b;
-        public static final int inactive=0x7f07000a;
-        public static final int menu_settings=0x7f070004;
-        public static final int missed_call_subject_received=0x7f07001d;
-        public static final int pref_calls_email_forward=0x7f070018;
-        public static final int pref_calls_email_forward_summary=0x7f070019;
-        public static final int pref_calls_forward=0x7f07001a;
-        public static final int pref_calls_forward_summary=0x7f07001b;
-        public static final int pref_email=0x7f07000c;
-        public static final int pref_email_summary=0x7f07000d;
-        public static final int pref_password=0x7f070010;
-        public static final int pref_password_summary=0x7f070011;
-        public static final int pref_receiver=0x7f070012;
-        public static final int pref_receiver_summary=0x7f070013;
-        public static final int pref_sms_email_forward=0x7f070014;
-        public static final int pref_sms_email_forward_summary=0x7f070015;
-        public static final int pref_sms_forward=0x7f070016;
-        public static final int pref_sms_forward_summary=0x7f070017;
-        public static final int pref_username=0x7f07000e;
-        public static final int pref_username_summary=0x7f07000f;
-        public static final int service_name=0x7f070001;
-        public static final int sms_received=0x7f07001c;
-        public static final int start=0x7f070005;
-        public static final int stop=0x7f070006;
-        public static final int txt_version=0x7f070003;
+        public static final int about=0x7f060002;
+        public static final int active=0x7f060005;
+        public static final int app_name=0x7f060000;
+        public static final int inactive=0x7f060006;
+        public static final int menu_settings=0x7f060004;
+        public static final int missed_call_subject_received=0x7f060018;
+        public static final int pref_calls_email_forward=0x7f060013;
+        public static final int pref_calls_email_forward_summary=0x7f060014;
+        public static final int pref_calls_forward=0x7f060015;
+        public static final int pref_calls_forward_summary=0x7f060016;
+        public static final int pref_email=0x7f060007;
+        public static final int pref_email_summary=0x7f060008;
+        public static final int pref_password=0x7f06000b;
+        public static final int pref_password_summary=0x7f06000c;
+        public static final int pref_receiver=0x7f06000d;
+        public static final int pref_receiver_summary=0x7f06000e;
+        public static final int pref_sms_email_forward=0x7f06000f;
+        public static final int pref_sms_email_forward_summary=0x7f060010;
+        public static final int pref_sms_forward=0x7f060011;
+        public static final int pref_sms_forward_summary=0x7f060012;
+        public static final int pref_username=0x7f060009;
+        public static final int pref_username_summary=0x7f06000a;
+        public static final int service_name=0x7f060001;
+        public static final int sms_received=0x7f060017;
+        public static final int txt_version=0x7f060003;
     }
     public static final class style {
         /** 
@@ -91,14 +81,13 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f080000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f080001;
+        public static final int AppTheme=0x7f070001;
     }
     public static final class xml {
-        public static final int appwidget_info=0x7f040000;
-        public static final int prefs=0x7f040001;
+        public static final int prefs=0x7f040000;
     }
 }
